fd65b0352cf42d3306063e3575d84876fa6c5207
shishupalkumar0926@gmail.com <shishupalkumar0926@gmail.com>
2024-11-27 14:15:20 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([3,4,5,2,5,2])
+print(arr)
+print(type(arr))
+
+print(np.array([3,5,2,5]))
+print(np.__version__)
+print(type(np.array((3,5,6,3,6))))
+
+# print(type((54,5)))  # It is a normal tuple 
